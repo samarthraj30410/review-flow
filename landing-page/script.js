2a306d90ffec1c91a7a197076b3251bfa5b2a372
samarthraj30410 <samarth.raj30410@gmail.com>
2026-07-30 00:19:01 +0530

    Emojis to SVG Icons, fixed the href of templates
    Removed some Useless SVG

--- landing-page/script.js
+++ landing-page/script.js
@@ -327,7 +327,7 @@ function initGalleryLightbox() {
   overlay.className = 'lightbox-overlay';
   overlay.innerHTML = `
     <div class="lightbox-content">
-      <button class="lightbox-close" aria-label="Close lightbox">✕</button>
+      <button class="lightbox-close" aria-label="Close lightbox"><svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="white"/></svg></button>
       <button class="lightbox-nav prev" aria-label="Previous image">‹</button>
       <button class="lightbox-nav next" aria-label="Next image">›</button>
       <img src="" alt="" />
